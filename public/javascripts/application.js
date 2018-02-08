4a4737b8232b6d0215bb7db85932481860f198fe
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-02-08 16:35:35 +0100

    Feat: show route

---
+++ public/javascripts/application.js
@@ -0,0 +1,70 @@
+const mapsAPI = new APIMaps();
+// const handlerAPI = new APIHandler("http://localhost:3000");
+
+function startMap() {
+  mapsAPI.startMap();
+}
+
+$(document).ready(() => {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function startMap() {
+//   let map;
+//   let currentMarker;
+//   let ironhackBCN = {
+//     lat: 41.3977381,
+//     lng: 2.190471916
+//   };
+//   map = new google.maps.Map(
+//     document.getElementById('map'), {
+//       zoom: 15,
+//       center: ironhackBCN
+//     }
+//   );
+//   currentMarker= myMarker(map);
+//   getPosition(map, currentMarker);
+// }
+
+// function myMarker(map) {
+//   return new google.maps.Marker({
+//     position: {
+//       lat: 41.3977381,
+//       lng: 2.190471916
+//     },
+//     map: map,
+//     title: "I'm here"
+//   });
+// };
+
+// function getPosition(map, currentMarker) {
+//   google.maps.event.addListener(map, "click", function (e) {
+//     currentMarker.setMap(null);
+//     currentMarker = new google.maps.Marker({
+//       position: e.latLng,
+//       map: map,
+//       title: "I'm here"
+//     });
+//     $('#lat').val(e.latLng.lat);
+//     $('#lng').val(e.latLng.lng);
+//   });
+// }
